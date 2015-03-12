eaf5195bf7f8df522c3ceda6253cee9c37e082af
tom <tom.waite@me.com>
2015-03-12 06:05:13 +0000

    more comments

--- week3/easy-chat/public/js/basic_chat.js
+++ week3/easy-chat/public/js/basic_chat.js
@@ -5,10 +5,8 @@ $(function(){
 	// connect to the socket
 	var socket = io.connect('/socket');
 
-	// some more jquery objects
-	var chatNickname = $(".nickname-chat"),
-		leftNickname = $(".nickname-left"),
-		chatForm = $("#chatform"),
+	// some jquery objects
+	var chatForm = $("#chatform"),
 		textarea = $("#message"),
 		chats = $("#chats");  //was a .chats....
 
@@ -26,9 +24,7 @@ $(function(){
 	
 	
 	textarea.keypress(function(e){
-
 		// Submit the form on enter
-
 		if(e.which == 13) {
 			e.preventDefault();
 			chatForm.trigger('submit');
@@ -39,7 +35,8 @@ $(function(){
 	chatForm.on('submit', function(e){
 
 		e.preventDefault();
-		// Create a new chat message and display it directly		
+		// Create a new chat message and display it directly
+		//Question: what value does name have?
 		createChatMessage(textarea.val(), name, moment());
 		
 		// Send the message to the other person in the chat
@@ -57,32 +54,7 @@ $(function(){
 		chats.animate({
 		scrollTop:chats[0].scrollHeight - chats.height()},100)
 
-		
 	}
 	
-	
-// bring in Eliza so she can talk to us
-var ElizaBot = require('./node-eliza/elizabot.js')
-
-	// Initialize a new socket.io application, named 'chat'
-	var chat = io.of('/socket').on('connection', function (socket) {
-		var eliza = new ElizaBot
-		eliza.memSize = 1024
-		// Handle the sending of messages
-		socket.on('msg', function(data){
-			console.log(data.msg)
-			
-			//we received a msg from the human, 
-			var reply = eliza.transform(data.msg)
-			console.log(reply)
-			// When the server receives a message, it sends it to the other person in the room.
-			//socket.broadcast.to(socket.room).emit('receive', {msg: reply, user: 'Eliza', img: data.img});
-			//because we are eliza we bounce back to the only person in the room with a simple emit
-			socket.emit('receive', {msg: reply, user: 'Eliza', img: data.img});
-		});
-	});
-
-	
-
 	
 });
--- week3/simple_router.js
+++ week3/simple_router.js
@@ -1,10 +1,25 @@
 var express = require('express')
 var app = express()
 
+var x = 100
+
 // when we get asked for the root with a GET, reply Hello world
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+
+//app.get(string, function_to_do_when_that_string_is_asked_for)
+app.get('/', 
+		function (req, res) 
+		{
+					res.send('Hello World!')
+		}
+		)
+		
+app.get('/Dune', 
+		function (req, res) 
+		{		console.log(req)
+				console.log("someone just connected!!")
+					res.send('Hello Dune!')
+		}
+		)
 
 //start listening at port 3000 on this computer 
 //when we attach to the port and start listening, call the function that says what 
